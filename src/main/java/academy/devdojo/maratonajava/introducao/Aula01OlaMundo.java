9748d61b326c73ddc896ca36a7c16b493df860a4
Matheus <145023775+matheuscx1@users.noreply.github.com>
2025-02-23 15:59:14 -0300

    Projeto de estudos inicial

---
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula01OlaMundo.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Comentario <b>javadoc</b>
+ */
+public class Aula01OlaMundo {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+    }
+}
+
+//Comentarios
+
+/*
+Comentarios para multiplas linhas
+ */
---
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        //Tipos primitivos
+        //int, double, float, char, byte short, long, boolean (todos minusculos)
+
+
+    }
+}
