5e7e03764cf66434c2bb6e9e1910c98439acb121
Matheus <145023775+matheuscx1@users.noreply.github.com>
2025-02-24 21:08:03 -0300

    testando if else

---
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais02.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais02 {
+    public static void main(String[] args) {
+        // idade < 15 categoria junevil
+        // idade >= 15 && idade < 18 categoria juvenil
+        // idade >= 18 categoria adulto
+
+        int idade = 17;
+        String categoria;
+
+        if(idade < 15){
+            categoria = "Categoria infantil";
+        }else if(idade >= 15 ){
+            categoria = "Categoria juvenil";
+        }else{
+            categoria = "Categoria adulto";
+        }
+        System.out.println(categoria);
+
+        //Quando usar if separado e if-else?
+        // Use if separado quando as condições são independentes (ou seja, mais de uma pode ser true ao mesmo tempo).
+        // Use if-else quando as condições são mutuamente exclusivas (ou seja, só um bloco pode ser verdadeiro por vez).
+
+
+
+    }
+}
