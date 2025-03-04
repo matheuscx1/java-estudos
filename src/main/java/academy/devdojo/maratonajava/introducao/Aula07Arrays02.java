689c3937335572d361a9fbef7afce084c2a63630
Matheus <145023775+matheuscx1@users.noreply.github.com>
2025-03-04 18:32:30 -0300

    Atualizando exemplo de Arrays

--- src/main/java/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -6,10 +6,19 @@ public class Aula07Arrays02 {
         //char '\u0000 ' ' '
         //boolean false
         // String null
-        int[] idades = new int[3];
-        System.out.println(idades[0]);
-        System.out.println(idades[1]);
-        System.out.println(idades[3]);
+        String[] nomes = new String[3];
+        nomes[0] = "Biriri";
+        nomes[1] = "Xurimini";
+        nomes[2] = "JIJI";
 
+        for (int i = 0; i < 3; i++) {
+            System.out.println(nomes[i]);
+        }
+        // percorre a quatidade do array
+
+        for (int i = 0; i <nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
+        //Adicionando variavel + .length, voce acessa a quatidade do array do mesmo jeito
     }
 }
