29c74da69ea9129cf17663151d40e25358decead
Matheus <145023775+matheuscx1@users.noreply.github.com>
2025-03-04 17:47:00 -0300

    um pouco mais sobre arrays

--- src/main/java/academy/devdojo/maratonajava/introducao/Aula07Arrays.java
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula07Arrays.java
@@ -4,7 +4,10 @@ public class Aula07Arrays {
     public static void main(String[] args) {
         //entidade do tipo reference, esta fazendo referencia ao um objeto em memoria que tem 3 espaços
         int[] idades = new int[3];
-
+        idades[0] = 21;
+        idades[1] = 15;
+        idades[2] = 11;
+        idades[3] = 11;//Invalido, pois o array possui apenas 3 posicoes
         System.out.println(idades);
     }
 }
