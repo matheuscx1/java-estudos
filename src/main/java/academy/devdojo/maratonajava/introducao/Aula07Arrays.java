41dd4f6984e34b1d2dd0e28aacaebc1fec56d43a
Matheus <145023775+matheuscx1@users.noreply.github.com>
2025-03-04 17:01:38 -0300

    Arrays

---
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula07Arrays.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays {
+    public static void main(String[] args) {
+        //entidade do tipo reference, esta fazendo referencia ao um objeto em memoria que tem 3 espaços
+        int[] idades = new int[];
+        System.out.println(idades);
+    }
+}
