d3f16ef96190758db77aa963157956b16888d981
Matheus <145023775+matheuscx1@users.noreply.github.com>
2025-03-06 22:01:02 -0300

    Formas de criar um array

---
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula07Arrays03.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays03 {
+    public static void main(String[] args) {// 3 formas de fazer array
+        int[] numeros = new int[3];
+        int[] numeros2 = {1,2,3,4,5};
+        int[] numeros3 =  new int[]{1,2,3,4,5};
+
+        for (int i = 0; i < numeros2.length ; i++) {
+            System.out.println(numeros3[i]);
+
+        }
+    }
+}
