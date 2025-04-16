b71a8eba155ac787055ec4935b4a08a0d3def6f1
Matheus <145023775+matheuscx1@users.noreply.github.com>
2025-04-15 22:47:33 -0300

    POO java

--- src/main/java/academy/devdojo/maratonajava/introducao/Arrays08Multidimencionais01.java
+++ src/main/java/academy/devdojo/maratonajava/introducao/Arrays08Multidimencionais01.java
@@ -5,17 +5,13 @@ public class Arrays08Multidimencionais01 {
         int[][] dias = new int[3][3];
 
         //   i  j
-        dias[0][0] = 1;
-        dias[0][1] = 2;
-        dias[0][2] = 3;
+        dias[0][0] = 31;
+        dias[0][1] = 28;
+        dias[0][2] = 31;
 
-        dias[1][0] = 4;
-        dias[1][1] = 5;
-        dias[1][2] = 6;
-
-        dias[2][0] = 7;
-        dias[2][1] = 8;
-        dias[2][2] = 9;
+        dias[1][0] = 31;
+        dias[1][1] = 28;
+        dias[1][2] = 31;
 
         for (int i = 0; i < dias.length ; i++) {
             for (int j = 0; j < dias[i].length ; j++) {
