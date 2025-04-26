574597e42ae54b6c0aa32e5522e2c571dfcf04db
Matheus <145023775+matheuscx1@users.noreply.github.com>
2025-04-25 22:41:51 -0300

    conceitos basicos java

---
+++ src/main/java/academy/devdojo/maratonajava/BIntroducaometodosPOO/ConceitosBasicosJava.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.BIntroducaometodosPOO;
+
+public class ConceitosBasicosJava {
+    //Objetos: são como "coisas" no seu programa. Um "Estudante" é um objeto, e esse objeto pode ter "atributos" como nome, idade e sexo.
+
+    //Métodos: são como "ações" que essas coisas (objetos) podem fazer. O método imprime() é uma ação que pega os dados de um objeto (como o nome, idade, etc.) e os exibe.
+
+    //Parâmetros: são como "informações" que você manda para um método. No caso do imprime(), o parâmetro é um objeto Estudante que você passa para o método fazer algo com ele.
+
+    //Passagem por referência: quando você "passa um objeto para um método", o método não cria uma cópia desse objeto. Ele usa a "localização" dele na memória para acessar e modificar os dados do objeto. Isso é chamado de passagem por referência.
+}
