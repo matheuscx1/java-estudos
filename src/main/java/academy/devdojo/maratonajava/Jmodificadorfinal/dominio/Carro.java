13d06b50f3d46381882be5f30e64307c3cd46bde
matheuscx1 <matheuscx1@hotmail.com>
2025-05-31 22:58:47 -0300

    modificador final tipo referencia

--- src/main/java/academy/devdojo/maratonajava/Jmodificadorfinal/dominio/Carro.java
+++ src/main/java/academy/devdojo/maratonajava/Jmodificadorfinal/dominio/Carro.java
@@ -2,8 +2,11 @@ package academy.devdojo.maratonajava.Jmodificadorfinal.dominio;
 
 public class Carro {
     private String nome;
+    //static pertence a classe, não ao objeto
     public static final double VELOCIDADE_LIMITE;
     //Quando vc marca um primitivo como final, ele nunca pode ser mudado depois disso
+    // Constantes static + final precisam usar capslock
+    //se remover o static não vai funcionar, pois precisaria chamar o objeto Carro.
 
     static {
         VELOCIDADE_LIMITE = 250;
---
+++ src/main/java/academy/devdojo/maratonajava/Jmodificadorfinal/dominio/Comprador.java
@@ -0,0 +1,4 @@
+package academy.devdojo.maratonajava.Jmodificadorfinal.dominio;
+
+public class Comprador {
+}
